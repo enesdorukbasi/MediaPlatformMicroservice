890bed9cf0ebb9f7bfafeff9439b38108cc5a816
Enes Dorukbaşı <51122010+enesdorukbasi@users.noreply.github.com>
2026-08-24 17:59:00 +0300

    Added EF Core MongoDB context and entity configurations

---
+++ src/services/catalog/MediaPlatformMicroservice.Catalog.API/Repositories/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MediaPlatformMicroservice.Catalog.API.Features.Categories;
+using MediaPlatformMicroservice.Catalog.API.Features.Courses;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+using System.Reflection;
+
+namespace MediaPlatformMicroservice.Catalog.API.Repositories;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ src/services/catalog/MediaPlatformMicroservice.Catalog.API/Repositories/Configurations/CourseEntityConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using MediaPlatformMicroservice.Catalog.API.Features.Courses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MongoDB.EntityFrameworkCore.Extensions;
+using System.Reflection.Emit;
+
+namespace MediaPlatformMicroservice.Catalog.API.Repositories.Configurations;
+
+public class CourseEntityConfiguration : IEntityTypeConfiguration<Course>
+{
+    public void Configure(EntityTypeBuilder<Course> builder)
+    {
+        builder.ToCollection("courses");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever();
+        builder.Property(x => x.Name).HasElementName("name").HasMaxLength(100);
+        builder.Property(x => x.Description).HasElementName("description").HasMaxLength(1000);
+        builder.Property(x => x.CategoryId).HasElementName("categoryId");
+        builder.Property(x => x.Price).HasElementName("price");
+        builder.Property(x => x.CreatedAt).HasElementName("createdAt");
+        builder.Property(x => x.Picture).HasElementName("picture");
+        builder.Property(x => x.UserId).HasElementName("userId");
+        builder.Ignore(x => x.Category);
+
+
+        builder.OwnsOne(x => x.Feature, feature =>
+        {
+            feature.HasElementName("feature");
+            feature.Property(x => x.Duration).HasElementName("duration");
+            feature.Property(x => x.Rating).HasElementName("rating");
+            feature.Property(x => x.EducatorFullName).HasElementName("educatorFullName").HasMaxLength(100);
+        });
+    }
+}
